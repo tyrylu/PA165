99c79c64f28743a84033ad2c69f2a6764696b92f
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-09-24 19:01:01 +0200

    The rest of the implementation, hopefully.

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -30,7 +31,23 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 
 	@Override
 	public void addCarToStorage(Car car) {
+		if(car.getPrice() < 1) {
+			throw new IllegalArgumentException();
+	}
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
+	}
+
+	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		for(Map.Entry<CarTypes,List<Car>> entry : CarShopStorage.getInstancce().getCars().entrySet()) {
+			int idx = entry.getValue().indexOf(car);
+			if(idx == -1) {
+				throw new RequestedCarNotFoundException("Not found.");
+			}
+			entry.getValue().remove(idx);
+			
+		}
+		
 	}
 
 }
