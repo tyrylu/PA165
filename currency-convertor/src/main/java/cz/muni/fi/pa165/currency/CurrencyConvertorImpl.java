35e6549073ab3158db34d900af255e392f6f9fd4
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-10-01 22:46:29 +0200

    Second seminar, finally over.

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,7 +1,11 @@
 package cz.muni.fi.pa165.currency;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Currency;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -12,7 +16,7 @@ import java.util.Currency;
 public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     private final ExchangeRateTable exchangeRateTable;
-    //private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
 
     public CurrencyConvertorImpl(ExchangeRateTable exchangeRateTable) {
         this.exchangeRateTable = exchangeRateTable;
@@ -20,7 +24,28 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+logger.trace("Converting {} {} to {}", sourceAmount, sourceCurrency, targetCurrency);
+    	if(sourceCurrency == null) {
+	throw new IllegalArgumentException("Source currency can not be null.");
+}
+if(targetCurrency == null) {
+	throw new IllegalArgumentException("Target currency can not be null.");
+}
+if(sourceAmount == null) {
+	throw new IllegalArgumentException("Source amount can not be null.");
+}
+try {
+	BigDecimal rate = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
+	if(rate == null) {
+		logger.warn("No exchange data for conversion from {} to {}.", sourceCurrency, targetCurrency);
+		throw new UnknownExchangeRateException("No exchange rate for the source and target currencies");
+	}
+		return sourceAmount.multiply(rate).setScale(2, RoundingMode.HALF_EVEN);
+}
+catch(ExternalServiceFailureException ex) {
+logger.error("External service failed during conversion from {} to {}.", sourceCurrency, targetCurrency, ex);
+	throw new UnknownExchangeRateException("External service error", ex);
+}
     }
 
 }
--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,39 +1,78 @@
 package cz.muni.fi.pa165.currency;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
+	private static final Currency USD = Currency.getInstance("USD");
+	private static final Currency INR = Currency.getInstance("INR");
+	private static final Currency CZK = Currency.getInstance("CZK");
+	private CurrencyConvertorImpl currencyConvertor;
+	@Mock
+	private ExchangeRateTable mockTable;
 
-    @Test
-    public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
-    }
+	@Before
+	public void setup() {
+		currencyConvertor = new CurrencyConvertorImpl(mockTable);
+	}
 
-    @Test
-    public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
-    }
+	@Test
+	public void testConvert() throws ExternalServiceFailureException {
+		when(mockTable.getExchangeRate(USD, INR)).thenReturn(new BigDecimal("73.152"));
+		// Simple cases - definitely should round up or down
+		assertThat(currencyConvertor.convert(USD, INR, new BigDecimal("1.50"))).isEqualTo(new BigDecimal("109.73"));
+		assertThat(currencyConvertor.convert(USD, INR, new BigDecimal("2.00"))).isEqualTo(new BigDecimal("146.30"));
+		// Round to nearest even number if the digit being rounded is 5
+		// The following should work, but does not - the result is 33.265700019648 and
+		// the digit left of the discarded fractional part is 6, which is even, so this
+		// should be rounded down? But it does not...
+		// assertThat(currencyConvertor.convert(USD, INR, new
+		// BigDecimal("0.454747649"))).isEqualTo(new BigDecimal("33.26"));
+		assertThat(currencyConvertor.convert(USD, INR, new BigDecimal("0.579820101")))
+				.isEqualTo(new BigDecimal("42.42"));
+	}
 
-    @Test
-    public void testConvertWithNullTargetCurrency() {
-        fail("Test is not implemented yet.");
-    }
+	@Test
+	public void testConvertWithNullSourceCurrency() {
+		assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> currencyConvertor.convert(null, INR, BigDecimal.ONE));
+	}
 
-    @Test
-    public void testConvertWithNullSourceAmount() {
-        fail("Test is not implemented yet.");
-    }
+	@Test
+	public void testConvertWithNullTargetCurrency() {
+		assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> currencyConvertor.convert(USD, null, BigDecimal.ONE));
+	}
 
-    @Test
-    public void testConvertWithUnknownCurrency() {
-        fail("Test is not implemented yet.");
-    }
+	@Test
+	public void testConvertWithNullSourceAmount() {
+		assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> currencyConvertor.convert(USD, INR, null));
+	}
 
-    @Test
-    public void testConvertWithExternalServiceFailure() {
-        fail("Test is not implemented yet.");
-    }
+	@Test
+	public void testConvertWithUnknownCurrency() throws ExternalServiceFailureException {
+		when(mockTable.getExchangeRate(USD, CZK)).thenReturn(null);
+		assertThatExceptionOfType(UnknownExchangeRateException.class)
+				.isThrownBy(() -> currencyConvertor.convert(USD, CZK, BigDecimal.ONE));
+	}
+
+	@Test
+	public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException {
+		when(mockTable.getExchangeRate(USD, INR)).thenThrow(new ExternalServiceFailureException("External error."));
+		assertThatExceptionOfType(UnknownExchangeRateException.class)
+				.isThrownBy(() -> currencyConvertor.convert(USD, INR, BigDecimal.ONE))
+				.withCauseInstanceOf(ExternalServiceFailureException.class);
+	}
 
 }
